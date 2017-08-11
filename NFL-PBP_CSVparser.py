0dbb47bc3016d562f5e70eb294ae0e33824d43a7
Jonathan Mooneyham <mooneyj3@wwu.edu>
2017-08-10 17:08:05 -0700

    Initial commit

---
+++ NFL-PBP_CSVparser.py
@@ -0,0 +1,4 @@
+##
+# This is a simple file parser to convert the spreadsheets from Bryan Povlinski to a format I need
+# https://www.spreadsheetsports.com/free-tools/2013-nfl-play-play-data-excel/
+##
